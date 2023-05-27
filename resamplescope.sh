d4d97c634ceade04fa1ac018ec5f88a2a5f1eeed
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-05-27 19:41:44 +0200

    Test other formats as well

--- resamplescope.sh
+++ resamplescope.sh
@@ -16,17 +16,17 @@ for filter in "${magick_filters[@]}"; do
   ./rscope -name "$filter resize with ImageMagick" -nologo pd_magick_$filter.png ../output/pd_magick_$filter-out.png
 done
 
-vips cast pd.png pd.v double
-vips cast pdr.png pdr.v double
+formats=(char ushort short uint int float complex double dpcomplex)
+kernel=lanczos3
 
-vips_kernels=(nearest linear cubic mitchell lanczos2 lanczos3)
-for kernel in "${vips_kernels[@]}"; do
-  vips reduceh pd.v pd_vips_$kernel.png 1.003603604 --kernel $kernel
-  vips reducev pdr.v pdr_vips_$kernel.png 1.003603604 --kernel $kernel --vips-novector
-  vips reducev pdr.v pdr_vips_vector_$kernel.png 1.003603604 --kernel $kernel
-  ./rscope -name "$kernel reduceh with libvips" -nologo pd_vips_$kernel.png ../output-patch/pd_vips_$kernel-out.png
-  ./rscope -name "$kernel reducev with libvips" -nologo -r pdr_vips_$kernel.png ../output-patch/pdr_vips_$kernel-out.png
-  ./rscope -name "$kernel reducev with libvips (vector path)" -nologo -r pdr_vips_vector_$kernel.png ../output-patch/pdr_vips_vector_$kernel-out.png
+for format in "${formats[@]}"; do
+  vips cast pd.png pd.v $format
+  vips cast pdr.png pdr.v $format
+
+  vips reduceh pd.v pd_vips_${kernel}_$format.png 1.003603604 --kernel $kernel
+  vips reducev pdr.v pdr_vips_${kernel}_$format.png 1.003603604 --kernel $kernel --vips-novector
+  ./rscope -name "$kernel reduceh with libvips" -nologo pd_vips_${kernel}_$format.png ../output-patch/pd_vips_${kernel}_$format-out.png
+  ./rscope -name "$kernel reducev with libvips" -nologo -r pdr_vips_${kernel}_$format.png ../output-patch/pdr_vips_${kernel}_$format-out.png
 done
 
 for i in "${!vips_kernels[@]}"; do 
